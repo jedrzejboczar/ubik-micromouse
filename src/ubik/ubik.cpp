4f7effb8b275053afa7723570d4bb4d5f336cd80
Jędrzej Boczar <yendreij@gmail.com>
2018-11-13 13:47:46 +0100

    Better button monitoring

--- src/ubik/ubik.cpp
+++ src/ubik/ubik.cpp
@@ -29,6 +29,44 @@ extern "C" void callback_timer_period_elapsed(TIM_HandleTypeDef *htim) {
     }
 }
 
+
+void monitor(void *) {
+    auto check_button = []() {
+        return HAL_GPIO_ReadPin(SW_Start_GPIO_Port, SW_Start_Pin) == GPIO_PIN_SET;
+    };
+
+    bool regulation_on = false;
+    int min_button_delay_ms = 500; // delay between subsequent button presses
+
+    auto last_start = xTaskGetTickCount();
+    int last_button_press = std::numeric_limits<int>::min();
+    while (1) {
+        // when the button has not been used for at least 1 second, to avoid double-press
+        if (xTaskGetTickCount() - last_button_press > pdMS_TO_TICKS(min_button_delay_ms)) {
+            // sample the button for <100 ms
+            int button_on_count = 0;
+            for (int i = 0; i < 10; i++) {
+                if (check_button())
+                    button_on_count++;
+                vTaskDelay(8);
+            }
+
+            // register button press when there were 70-100 % positives
+            if (button_on_count > 6) {
+                // toggle regulation state on button press
+                regulation_on = !regulation_on;
+                logging::printf(100, "Setting regulation state: %s\n", regulation_on ? "ON" : "OFF");
+                movement::regulator::set_enabled(regulation_on);
+
+                // reset button timer
+                last_button_press = xTaskGetTickCount();
+            }
+        }
+
+        vTaskDelayUntil(&last_start, pdMS_TO_TICKS(100));
+    }
+}
+
 void set_target_position_task(void *) {
     using constants::deg2rad;
     movement::Controller controller(1000);
@@ -39,31 +77,14 @@ void set_target_position_task(void *) {
     float acc_ang = deg2rad(400);
 
     while (1) {
-        logging::printf(100, "Press button to start...\n");
-        while (HAL_GPIO_ReadPin(SW_Start_GPIO_Port, SW_Start_Pin) == GPIO_PIN_RESET)
-            vTaskDelay(50);
-
-        logging::printf(100, "Starting...\n");
-        movement::regulator::set_enabled(true);
-        vTaskDelay(500);
-        while (HAL_GPIO_ReadPin(SW_Start_GPIO_Port, SW_Start_Pin) == GPIO_PIN_RESET) {
-            controller.move_turn(deg2rad(45), vel_ang, acc_ang);   controller.reset();
-            controller.move_turn(deg2rad(-90), vel_ang, acc_ang);  controller.reset();
-            controller.move_turn(deg2rad(45), vel_ang, acc_ang);   controller.reset();
-            controller.move_line(.20, vel_lin, acc_lin);           controller.reset();
-            controller.move_turn(deg2rad(180), vel_ang, acc_ang);  controller.reset();
-            controller.move_line(.20, vel_lin, acc_lin);           controller.reset();
-            controller.move_turn(deg2rad(-180), vel_ang, acc_ang); controller.reset();
-        }
-
-        movement::regulator::set_enabled(false);
-        logging::printf(100, "Stopped.\n");
-        vTaskDelay(3000);
-    }
-
-
-    while(1) {
-        vTaskDelay(1000);
+        logging::printf(50, "Next cycle\n");
+        controller.move_turn(deg2rad(45), vel_ang, acc_ang);   controller.reset();
+        controller.move_turn(deg2rad(-90), vel_ang, acc_ang);  controller.reset();
+        controller.move_turn(deg2rad(45), vel_ang, acc_ang);   controller.reset();
+        controller.move_line(.20, vel_lin, acc_lin);           controller.reset();
+        controller.move_turn(deg2rad(180), vel_ang, acc_ang);  controller.reset();
+        controller.move_line(.20, vel_lin, acc_lin);           controller.reset();
+        controller.move_turn(deg2rad(-180), vel_ang, acc_ang); controller.reset();
     }
 }
 
@@ -86,9 +107,11 @@ void run() {
     bool all_created = true;
     all_created &= xTaskCreate(set_target_position_task, "Setter",
             configMINIMAL_STACK_SIZE * 3, nullptr, 2, nullptr) == pdPASS;
+    all_created &= xTaskCreate(monitor, "Monitor",
+            configMINIMAL_STACK_SIZE * 2, (void *) nullptr, 3, nullptr) == pdPASS;
     all_created &= xTaskCreate(movement::regulator::regulation_task, "Regulator",
             configMINIMAL_STACK_SIZE * 2, nullptr, 4, nullptr) == pdPASS;
-    all_created &= xTaskCreate(logging::stats_monitor_task, "SysMonitor",
+    all_created &= xTaskCreate(logging::stats_monitor_task, "Stats",
             configMINIMAL_STACK_SIZE * 2, (void *) pdMS_TO_TICKS(10*1000), 1, nullptr) == pdPASS;
     configASSERT(all_created);
 
